fe553518a05c207e0b4412270baf7d79f6ed3da3
ndondoRugaba <sagemuho@gmail.com>
2022-01-13 18:41:20 +0200

    finished database configuration and everything went well

---
+++ src/main/java/com/myweb/MainController.java
@@ -0,0 +1,15 @@
+package com.myweb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("")
+    public String ShowHomePage(){
+      return "index";
+    }
+
+
+}
