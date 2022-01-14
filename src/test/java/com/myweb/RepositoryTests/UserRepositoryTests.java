b72961b17faa44f78f13b697adcf804f58ef8dd0
ndondoRugaba <sagemuho@gmail.com>
2022-01-14 07:49:59 +0200

    ovveride to string methods and add listALL with UpadeUser repository Test

--- src/test/java/com/myweb/RepositoryTests/UserRepositoryTests.java
+++ src/test/java/com/myweb/RepositoryTests/UserRepositoryTests.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 
+import java.util.Optional;
+
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Rollback(false)
@@ -23,13 +25,40 @@ public class UserRepositoryTests {
 
         User user = new User();
 
-        user.setEmail("sagamba@gmail.com");
-        user.setPassword("sagamaba123");
-        user.setFirstName("sagamba");
+        user.setEmail("rugamba@gmail.com");
+        user.setPassword("ruhgamba123");
+        user.setFirstName("rugamba");
         user.setLastName("sadate");
         User savedUser = repo.save(user);
         Assertions.assertThat(savedUser).isNotNull();
         Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
     }
+
+    @Test
+    public void listAll(){
+     Iterable<User> users = repo.findAll();
+     Assertions.assertThat(users).hasSizeGreaterThan(0);
+
+     for(User user:users){
+         System.out.println(user);
+     }
+    }
+
+@Test
+    public void testUpdate(){
+        Integer userId=1;
+    Optional<User> optionalUser = repo.findById(userId);
+
+    User user= optionalUser.get();
+    user.setEmail("muco@gmail.com");
+    user.setPassword("muco123");
+    repo.save(user);
+
+    User updatedUser = repo.findById(userId).get();
+    Assertions.assertThat(updatedUser.getPassword()).isEqualTo("muco123");
+
+}
+
+
 
 }
