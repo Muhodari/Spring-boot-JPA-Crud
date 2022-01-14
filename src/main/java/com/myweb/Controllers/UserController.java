e35558f10d98b134705ad6b2409f402c5ba72715
ndondoRugaba <sagemuho@gmail.com>
2022-01-14 09:50:45 +0200

    added new user functionality done

--- src/main/java/com/myweb/Controllers/UserController.java
+++ src/main/java/com/myweb/Controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -32,8 +33,9 @@ public class UserController {
     }
 
   @PostMapping("/users/save")
-  public String saveUser(User user){
+  public String saveUser(User user, RedirectAttributes ra){
    userService.save(user);
+   ra.addFlashAttribute("message","the user has been added successfully. ");
    return "redirect:/users";
   }
 
